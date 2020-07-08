c573676cd0313072b8668c49536e4eb7fe232a1c
Hadrien Pomès <hadrien.pomes@sbg-systems.com>
2020-07-08 17:02:13 +0200

    Update Licenses

--- external/sbgECom/src/commands/sbgEComCmdFeatures.h
+++ external/sbgECom/src/commands/sbgEComCmdFeatures.h
@@ -6,16 +6,27 @@
  *	\brief		This file implements SbgECom commands related to device features.
  *
  *	\section CodeCopyright Copyright Notice 
- *	Copyright (C) 2007-2013, SBG Systems SAS. All rights reserved.
- *	
- *	This source code is intended for use only by SBG Systems SAS and
- *	those that have explicit written permission to use it from
- *	SBG Systems SAS.
- *	
- *	THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
- *	KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- *	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
- *	PARTICULAR PURPOSE.
+ *  The MIT license
+ *  
+ *  Copyright (C) 2007-2020, SBG Systems SAS. All rights reserved.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
  */
 #ifndef __SBG_ECOM_CMD_FEATURES_H__
 #define __SBG_ECOM_CMD_FEATURES_H__
