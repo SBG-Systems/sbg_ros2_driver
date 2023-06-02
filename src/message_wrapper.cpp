30efec026d4766dcaca0ab14be2044e55d5e5fde
cledant <cledant@student.42.fr>
2023-06-02 14:27:47 +0200

    Fixed deprecated header warning

--- src/message_wrapper.cpp
+++ src/message_wrapper.cpp
@@ -3,7 +3,7 @@
 
 // ROS headers
 #include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/static_transform_broadcaster.h>
 
