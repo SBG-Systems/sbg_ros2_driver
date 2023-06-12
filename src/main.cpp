7647807f9b737502519dae2a1459a785e77fe5b7
cledant <cledant@student.42.fr>
2023-06-12 13:17:58 +0200

    Removed threaded subscription.

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,30 @@ using sbg::SbgDevice;
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::Node node_handle("sbg_device");
+  auto node_handle = std::make_shared<rclcpp::Node>("sbg_device");
 
   try
   {
     uint32_t loopFrequency;
 
-    RCLCPP_INFO(node_handle.get_logger(), "SBG DRIVER - Init node, load params and connect to the device.");
-    SbgDevice sbg_device(node_handle);
+    RCLCPP_INFO(node_handle->get_logger(), "SBG DRIVER - Init node, load params and connect to the device.");
+    SbgDevice sbg_device(*node_handle);
 
-    RCLCPP_INFO(node_handle.get_logger(), "SBG DRIVER - Initialize device for receiving data");
+    RCLCPP_INFO(node_handle->get_logger(), "SBG DRIVER - Initialize device for receiving data");
     sbg_device.initDeviceForReceivingData();
 
     loopFrequency = sbg_device.getUpdateFrequency();
-    RCLCPP_INFO(node_handle.get_logger(), "SBG DRIVER - ROS Node frequency : %u Hz", loopFrequency);
+    RCLCPP_INFO(node_handle->get_logger(), "SBG DRIVER - ROS Node frequency : %u Hz", loopFrequency);
     rclcpp::Rate loop_rate(loopFrequency);
 
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node_handle);
+
+    std::chrono::duration<int, std::nano> timeout(1000000);
     while (rclcpp::ok())
     {
       sbg_device.periodicHandle();
+      executor.spin_some(timeout);
       loop_rate.sleep();
     }
 
@@ -32,7 +37,7 @@ int main(int argc, char **argv)
   }
   catch (std::exception const& refE)
   {
-    RCLCPP_ERROR(node_handle.get_logger(), "SBG_DRIVER - %s", refE.what());
+    RCLCPP_ERROR(node_handle->get_logger(), "SBG_DRIVER - %s", refE.what());
   }
 
   return 0;
