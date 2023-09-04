80d0fd062047f8bf377130122d222353df13a6cd
cledant <cledant@student.42.fr>
2023-09-04 18:02:56 +0200

    Code indentation

--- src/sbg_utm.cpp
+++ src/sbg_utm.cpp
@@ -55,7 +55,7 @@ char Utm::getLetterDesignator() const
 
 std::array<double, 2> Utm::computeEastingNorthing(double latitude, double longitude) const
 {
-  constexpr double RADIANS_PER_DEGREE = M_PI/180.0;
+  constexpr double RADIANS_PER_DEGREE = M_PI / 180.0;
 
   // WGS84 Parameters
   constexpr double WGS84_A = 6378137.0;        // major axis
@@ -84,24 +84,25 @@ std::array<double, 2> Utm::computeEastingNorthing(double latitude, double longit
   LongOrigin = (zone_number_ - 1) * 6 - 180 + 3;
   LongOriginRad = LongOrigin * RADIANS_PER_DEGREE;
 
-  eccPrimeSquared = (eccSquared)/(1-eccSquared);
+  eccPrimeSquared = (eccSquared)/(1 - eccSquared);
 
-  N = a/sqrt(1-eccSquared*sin(LatRad)*sin(LatRad));
-  T = tan(LatRad)*tan(LatRad);
-  C = eccPrimeSquared*cos(LatRad)*cos(LatRad);
-  A = cos(LatRad)*(LongRad-LongOriginRad);
+  N = a/sqrt(1 - eccSquared * sin(LatRad) * sin(LatRad));
+  T = tan(LatRad) * tan(LatRad);
+  C = eccPrimeSquared * cos(LatRad) * cos(LatRad);
+  A = cos(LatRad) * (LongRad-LongOriginRad);
 
-  M = a*((1 - eccSquared/4      - 3*eccSquared*eccSquared/64     - 5*eccSquared*eccSquared*eccSquared/256)*LatRad
-         - (3*eccSquared/8   + 3*eccSquared*eccSquared/32    + 45*eccSquared*eccSquared*eccSquared/1024)*sin(2*LatRad)
-         + (15*eccSquared*eccSquared/256 + 45*eccSquared*eccSquared*eccSquared/1024)*sin(4*LatRad)
-         - (35*eccSquared*eccSquared*eccSquared/3072)*sin(6*LatRad));
+  M = a * ((1 - eccSquared/4      - 3*eccSquared*eccSquared/64    - 5*eccSquared*eccSquared*eccSquared/256) * LatRad
+              - (3*eccSquared/8   + 3*eccSquared*eccSquared/32    + 45*eccSquared*eccSquared*eccSquared/1024) * sin(2*LatRad)
+                                  + (15*eccSquared*eccSquared/256 + 45*eccSquared*eccSquared*eccSquared/1024) * sin(4*LatRad)
+                                  - (35*eccSquared*eccSquared*eccSquared/3072) * sin(6*LatRad));
 
-  double utm_easting = (double)(k0 * N * (A + (1 - T + C) * A * A * A / 6
-                                          + (5-18*T+T*T+72*C-58*eccPrimeSquared)*A*A*A*A*A/120)
-                                + 500000.0);
+  double utm_easting = (double)(k0*N*(A+(1-T+C)*A*A*A/6
+                       + (5-18*T+T*T+72*C-58*eccPrimeSquared)*A*A*A*A*A/120)
+                       + 500000.0);
 
-  double utm_northing = (double)(k0 * (M + N * tan(LatRad) * (A * A / 2 + (5 - T + 9 * C + 4 * C * C) * A * A * A * A / 24
-                                                              + (61-58*T+T*T+600*C-330*eccPrimeSquared)*A*A*A*A*A*A/720)));
+  double utm_northing = (double)(k0*(M+N*tan(LatRad)*(A*A/2+(5-T+9*C+4*C*C)*A*A*A*A/24
+                        + (61-58*T+T*T+600*C-330*eccPrimeSquared)*A*A*A*A*A*A/720)));
+
 
   if (latitude < 0)
   {
@@ -129,10 +130,22 @@ int Utm::computeZoneNumber(double latitude, double longitude)
   // Special zones for Svalbard
   if ( latitude >= 72.0 && latitude < 84.0 )
   {
-    if (      LongTemp >= 0.0  && LongTemp <  9.0 ) zoneNumber = 31;
-    else if ( LongTemp >= 9.0  && LongTemp < 21.0 ) zoneNumber = 33;
-    else if ( LongTemp >= 21.0 && LongTemp < 33.0 ) zoneNumber = 35;
-    else if ( LongTemp >= 33.0 && LongTemp < 42.0 ) zoneNumber = 37;
+    if ( LongTemp >= 0.0  && LongTemp < 9.0 )
+    {
+      zoneNumber = 31;
+    }
+    else if ( LongTemp >= 9.0  && LongTemp < 21.0 )
+    {
+      zoneNumber = 33;
+    }
+    else if ( LongTemp >= 21.0 && LongTemp < 33.0 )
+    {
+      zoneNumber = 35;
+    }
+    else if ( LongTemp >= 33.0 && LongTemp < 42.0 )
+    {
+      zoneNumber = 37;
+    }
   }
 
   return zoneNumber;
@@ -142,28 +155,91 @@ char Utm::computeLetterDesignator(double latitude)
 {
   char LetterDesignator;
 
-  if     ((84 >= latitude) && (latitude >= 72)) LetterDesignator = 'X';
-  else if ((72 > latitude) && (latitude >= 64)) LetterDesignator = 'W';
-  else if ((64 > latitude) && (latitude >= 56)) LetterDesignator = 'V';
-  else if ((56 > latitude) && (latitude >= 48)) LetterDesignator = 'U';
-  else if ((48 > latitude) && (latitude >= 40)) LetterDesignator = 'T';
-  else if ((40 > latitude) && (latitude >= 32)) LetterDesignator = 'S';
-  else if ((32 > latitude) && (latitude >= 24)) LetterDesignator = 'R';
-  else if ((24 > latitude) && (latitude >= 16)) LetterDesignator = 'Q';
-  else if ((16 > latitude) && (latitude >= 8)) LetterDesignator = 'P';
-  else if ((8 > latitude) && (latitude >= 0)) LetterDesignator = 'N';
-  else if ((0 > latitude) && (latitude >= -8)) LetterDesignator = 'M';
-  else if ((-8 > latitude) && (latitude >= -16)) LetterDesignator = 'L';
-  else if ((-16 > latitude) && (latitude >= -24)) LetterDesignator = 'K';
-  else if ((-24 > latitude) && (latitude >= -32)) LetterDesignator = 'J';
-  else if ((-32 > latitude) && (latitude >= -40)) LetterDesignator = 'H';
-  else if ((-40 > latitude) && (latitude >= -48)) LetterDesignator = 'G';
-  else if ((-48 > latitude) && (latitude >= -56)) LetterDesignator = 'F';
-  else if ((-56 > latitude) && (latitude >= -64)) LetterDesignator = 'E';
-  else if ((-64 > latitude) && (latitude >= -72)) LetterDesignator = 'D';
-  else if ((-72 > latitude) && (latitude >= -80)) LetterDesignator = 'C';
+  if ((84 >= latitude) && (latitude >= 72))
+  {
+    LetterDesignator = 'X';
+  }
+  else if ((72 > latitude) && (latitude >= 64))
+  {
+    LetterDesignator = 'W';
+  }
+  else if ((64 > latitude) && (latitude >= 56))
+  {
+    LetterDesignator = 'V';
+  }
+  else if ((56 > latitude) && (latitude >= 48))
+  {
+    LetterDesignator = 'U';
+  }
+  else if ((48 > latitude) && (latitude >= 40))
+  {
+    LetterDesignator = 'T';
+  }
+  else if ((40 > latitude) && (latitude >= 32))
+  {
+    LetterDesignator = 'S';
+  }
+  else if ((32 > latitude) && (latitude >= 24))
+  {
+    LetterDesignator = 'R';
+  }
+  else if ((24 > latitude) && (latitude >= 16))
+  {
+    LetterDesignator = 'Q';
+  }
+  else if ((16 > latitude) && (latitude >= 8))
+  {
+    LetterDesignator = 'P';
+  }
+  else if ((8 > latitude) && (latitude >= 0))
+  {
+    LetterDesignator = 'N';
+  }
+  else if ((0 > latitude) && (latitude >= -8))
+  {
+    LetterDesignator = 'M';
+  }
+  else if ((-8 > latitude) && (latitude >= -16))
+  {
+    LetterDesignator = 'L';
+  }
+  else if ((-16 > latitude) && (latitude >= -24))
+  {
+    LetterDesignator = 'K';
+  }
+  else if ((-24 > latitude) && (latitude >= -32))
+  {
+    LetterDesignator = 'J';
+  }
+  else if ((-32 > latitude) && (latitude >= -40))
+  {
+    LetterDesignator = 'H';
+  }
+  else if ((-40 > latitude) && (latitude >= -48))
+  {
+    LetterDesignator = 'G';
+  }
+  else if ((-48 > latitude) && (latitude >= -56))
+  {
+    LetterDesignator = 'F';
+  }
+  else if ((-56 > latitude) && (latitude >= -64))
+  {
+    LetterDesignator = 'E';
+  }
+  else if ((-64 > latitude) && (latitude >= -72))
+  {
+    LetterDesignator = 'D';
+  }
+  else if ((-72 > latitude) && (latitude >= -80))
+  {
+    LetterDesignator = 'C';
+  }
+  else
+  {
     // 'Z' is an error flag, the Latitude is outside the UTM limits
-  else LetterDesignator = 'Z';
+    LetterDesignator = 'Z';
+  }
   return LetterDesignator;
 }
 
