7e1e1eedc86671b25793ece73c58f4d0eeb5794a
Timon Mentink <timon.mentink@demcon.com>
2023-11-29 09:13:49 +0100

    Remove boost dependency
    (cherry picked from commit ab54c33f1e442c3737dd8e1c09a8b6f36c2c1afa)

--- src/sbg_device.cpp
+++ src/sbg_device.cpp
@@ -6,12 +6,6 @@
 #include <fstream>
 #include <ctime>
 
-// Boost headers
-#include <boost/lexical_cast.hpp>
-#include <boost/regex.hpp>
-#include <boost/thread/xtime.hpp>
-#include <boost/date_time/local_time/local_time.hpp>
-
 // SbgECom headers
 #include <version/sbgVersion.h>
 
@@ -19,16 +13,13 @@ using namespace std;
 using sbg::SbgDevice;
 
 // From ros_com/recorder
-std::string timeToStr() //rclcpp::WallTimer<std::function<void()>> ros_t) //TODO: FIXME
+std::string timeToStr()
 {
-  //(void)ros_t;
-  std::stringstream msg;
-  const boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
-  boost::posix_time::time_facet *const f = new boost::posix_time::time_facet("%Y-%m-%d-%H-%M-%S");
-  msg.imbue(std::locale(msg.getloc(),f));
-  msg << now;
-
-  return msg.str();
+    std::stringstream msg;
+    auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    auto tm = *std::localtime(&now);
+    msg << std::put_time(&tm, "%Y-%m-%d-%H-%M-%S");
+    return msg.str();
 }
 
 //
@@ -215,7 +206,7 @@ void SbgDevice::initSubscribers()
     auto rtcm_cb = [&](const rtcm_msgs::msg::Message::SharedPtr msg) -> void {
         this->writeRtcmMessageToDevice(msg);
     };
-    
+
     rtcm_sub_ = ref_node_.create_subscription<rtcm_msgs::msg::Message>(config_store_.getRtcmFullTopic(), 10, rtcm_cb);
   }
 }
@@ -455,7 +446,7 @@ void SbgDevice::exportMagCalibrationResults() const
   mag_results_stream << mag_calib_results_.matrix[3] << "\t" << mag_calib_results_.matrix[4] << "\t" << mag_calib_results_.matrix[5] << endl;
   mag_results_stream << mag_calib_results_.matrix[6] << "\t" << mag_calib_results_.matrix[7] << "\t" << mag_calib_results_.matrix[8] << endl;
 
-  output_filename = "mag_calib_" + timeToStr()/*(nullptrrclcpp::WallTimer::now())*/ + ".txt";
+  output_filename = "mag_calib_" + timeToStr() + ".txt";
   ofstream output_file(output_filename);
   output_file << mag_results_stream.str();
   output_file.close();
@@ -468,7 +459,7 @@ void SbgDevice::writeRtcmMessageToDevice(const rtcm_msgs::msg::Message::SharedPt
 {
   auto rtcm_data = msg->message;
   auto error_code = sbgInterfaceWrite(&sbg_interface_, rtcm_data.data(), rtcm_data.size());
-  
+
   if (error_code != SBG_NO_ERROR)
   {
     char error_str[256];
