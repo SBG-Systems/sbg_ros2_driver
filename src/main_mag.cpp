940c33a773c23e530fddffe2ae178f03933c71a4
Sander van Dijk <sander.vandijk@streetdrone.com>
2024-05-21 12:04:31 +0200

    Fix mag calibration node
    Make sure that the Node is actually created, and initialise and run it
    similar to how the main driver node is run.

--- src/main_mag.cpp
+++ src/main_mag.cpp
@@ -5,16 +5,32 @@ using sbg::SbgDevice;
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::Node::SharedPtr node_handle;
+  rclcpp::Node::SharedPtr node_handle = std::make_shared<rclcpp::Node>("sbg_device_mag");
 
   try
   {
+    uint32_t loopFrequency;
+
     RCLCPP_INFO(node_handle->get_logger(), "SBG DRIVER - Init node, load params and connect to the device");
     SbgDevice sbg_device(*node_handle);
-    
+
+    RCLCPP_INFO(node_handle->get_logger(), "SBG DRIVER - Initialize device for mag calibration");
     sbg_device.initDeviceForMagCalibration();
-    
-    rclcpp::spin(node_handle);
+
+    loopFrequency = sbg_device.getUpdateFrequency();
+    RCLCPP_INFO(node_handle->get_logger(), "SBG DRIVER - ROS Node frequency : %u Hz", loopFrequency);
+    rclcpp::Rate loop_rate(loopFrequency);
+
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node_handle);
+
+    std::chrono::duration<int, std::nano> timeout(1000000);
+    while (rclcpp::ok())
+    {
+      sbg_device.periodicHandle();
+      executor.spin_some(timeout);
+      loop_rate.sleep();
+    }
 
     return 0;
   }
